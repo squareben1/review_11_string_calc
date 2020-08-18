7ca0212aa8cd56e051fc5ff1ff0bf6201988ffdb
Squareben1 <bengittins@live.com>
2020-08-18 10:14:08 +0100

    Hardcoded single digit return

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,9 @@
 class StringCalc
   def string_calc(string)
-    ["", 0]
+    if string == "" 
+      ["", 0]
+    else 
+      ["1", 1]
+    end
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,4 +4,8 @@ describe StringCalc do
   it 'returns empty string & 0' do 
     expect(subject.string_calc("")).to eq ["", 0]
   end 
+
+  it 'returns single digit' do 
+    expect(subject.string_calc("1")).to eq ["1", 1]
+  end 
 end 
