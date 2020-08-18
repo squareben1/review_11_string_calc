a6fe3283d3cc57dcb5f397f200917f9e068648b7
Squareben1 <bengittins@live.com>
2020-08-18 12:40:34 +0100

    Multiply

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,11 +1,22 @@
 class StringCalc
+  def create_array(string)
+    split_string = string.split(" ")
+    num_arr = []
+    num_arr << split_string[0].to_i
+    num_arr << split_string[2].to_i
+  end
+
   def string_calc(string)
+    num_arr = create_array(string)
+    # arr[1:]
     if string == "" 
       ["", 0]
-    elsif string == "1"
-      [string, 1]
+    elsif string[2] == "+"
+      [string, (num_arr.sum)]
+    elsif string[2] == "*"
+      [string, (num_arr[0] * num_arr[1])]
     else
-      [string, 2]
+      [string, (num_arr[0] - num_arr[1])]
     end
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,9 +7,33 @@ describe StringCalc do
 
   it 'returns single digit' do 
     expect(subject.string_calc("1")).to eq ["1", 1]
+  end
+
+  it 'returns single diff digit' do 
+    expect(subject.string_calc("2")).to eq ["2", 2]
   end 
 
   it 'Adds two digits' do 
     expect(subject.string_calc("1 + 1")).to eq ["1 + 1", 2]
+  end 
+
+  it 'Adds two diff digits' do 
+    expect(subject.string_calc("1 + 2")).to eq ["1 + 2", 3]
+  end 
+
+  it 'subtracts two digits' do 
+    expect(subject.string_calc("1 - 1")).to eq ["1 - 1", 0]
+  end
+
+  it 'subtracts two digits' do 
+    expect(subject.string_calc("2 - 1")).to eq ["2 - 1", 1]
+  end
+
+  it 'subtracts two digits = negative' do #this was to check behaved as expected
+    expect(subject.string_calc("1 - 2")).to eq ["1 - 2", -1]
+  end
+
+  it 'Multiplies two digits' do 
+    expect(subject.string_calc("1 * 2")).to eq ["1 * 2", 2]
   end 
 end 
